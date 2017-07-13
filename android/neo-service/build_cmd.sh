84dafe69594c233d75926ba10ff28fe0dc9f8e98
viveks9 <vivek@obiai.tech>
2017-07-13 16:52:09 -0700

    Deriving ip from shell

--- android/neo-service/build_cmd.sh
+++ android/neo-service/build_cmd.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+if [ $# -ne 1 ]; then
+	echo "build_cmd SERVER_IP"
+	exit
+fi
 if [ -z $JAVA_HOME ]; then
 	export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home
 fi
+export WEBSOCKET_SERVER_IP=$1
+echo "Setting Web Socket Address to $WEBSOCKET_SERVER_IP"
 ./gradlew assembleRelease
 
 echo copying FROM  ../neo-service/app/build/outputs/aar/app-release.aar TO ./neo-service/
