4ac6c7ea5ab30a4df9e64d52446d2c7740b31c4f
viveks9 <vivek@obiai.tech>
2017-07-13 17:03:51 -0700

    Build script changes

--- android/neo-service/build_cmd.sh
+++ android/neo-service/build_cmd.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-	echo "build_cmd SERVER_IP"
-	exit
+if [ -z $WEBSOCKET_SERVER_IP ]; then
+	if [ $# -ne 1 ]; then
+		echo "build_cmd SERVER_IP"
+		exit
+	fi
 fi
 if [ -z $JAVA_HOME ]; then
 	export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home
 fi
-export WEBSOCKET_SERVER_IP=$1
+if [ ! -z $1 ]; then
+	export WEBSOCKET_SERVER_IP=$1
+fi
 echo "Setting Web Socket Address to $WEBSOCKET_SERVER_IP"
 ./gradlew assembleRelease
 
